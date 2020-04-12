293d940faec0aa8948f701e430fe8d87f5912e72
Ivan Nomokonov <nomokonov.vana@gmail.com>
2020-04-12 16:36:39 +0300

    WED-11 вывод результатов

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,20 +1,25 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
-import { Observable } from "rxjs";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/internal/operators';
 import { ApiService } from '../services/api.service';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
+  constructor(private api: ApiService, private router: Router) {}
 
-    constructor(private api: ApiService, private router: Router) { }
-
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
-        let token = sessionStorage.getItem('weddingUserToken');
-        if (token) {
-            return this.api.checkAccess();
-        }
-        this.router.navigate(['enter']);
-        return false;
-
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+    let token = sessionStorage.getItem('weddingUserToken');
+    if (token) {
+      return this.api.checkAccess().pipe(
+        tap((isAdmin) => {
+          if (!isAdmin) {
+            this.router.navigate(['enter']);
+          }
+        })
+      );
     }
+
+    return false;
+  }
 }
